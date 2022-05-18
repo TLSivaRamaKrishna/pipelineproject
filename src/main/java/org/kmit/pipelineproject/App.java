b2bfdebd4fadd16710081951883c1ee180573bc2
TLSivaRamaKrishna <drsivamca.mtech@gmail.com>
2022-05-18 12:59:25 +0530

    demo for pipelineproject

---
+++ src/main/java/org/kmit/pipelineproject/App.java
@@ -0,0 +1,13 @@
+package org.kmit.pipelineproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
